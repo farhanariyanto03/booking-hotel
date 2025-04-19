a498c4951b83d07ed9186c9bf8f3b6d51c15adbc
farhanariyanto03 <ariyantofarhan03@gmail.com>
2025-04-19 23:11:42 +0700

    Fetch: Setting prisma in project

--- component/login-button.tsx
+++ component/login-button.tsx
@@ -1,10 +1,16 @@
+import { signIn } from "@/auth";
 import { FaG } from "react-icons/fa6";
 
 export const LoginGoogleButton = () => {
   return (
-    <button className="flex items-center justify-center gap-2 w-full bg-orange-500 text-white font-medium py-3 px-6 text-base hover:bg-orange-600 cursor-pointer rounded-sm">
+    <form action={async () => {
+      "use server";
+      await signIn("google");
+    }}>
+      <button className="flex items-center justify-center gap-2 w-full bg-orange-500 text-white font-medium py-3 px-6 text-base hover:bg-orange-600 cursor-pointer rounded-sm">
       <FaG className="size-6" />
       Sign In With Google
     </button>
+    </form>
   );
 };
